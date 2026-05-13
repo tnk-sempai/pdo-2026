4aa550cf09c96847a7244462c6b8e023f167d08e
julia-dm <yulia.2508dmitruk@gmail.com>
2026-05-13 11:24:11 +0200

    Add connection

---
+++ stagiaires/Yuliia/06-exercice-classe1/controller/routerController.php
@@ -0,0 +1,9 @@
+<?php
+// stagiaires/Yuliia/06-exercice-classe1/controller/routerController.php
+
+# Importer le fichier model qui contient nos fonctions de la table commentaire
+
+# Création de notre connexion PDO (avec try catch)
+
+# suivant les actions utilisateur, appelez les vues.
+require ROOT_PROJECT."/view/homepage.html.php";
---
+++ stagiaires/Yuliia/06-exercice-classe1/model/CommentaireModel.php
@@ -0,0 +1,23 @@
+<?php
+# stagiaires/Michael/06-exercice-classe1/model/CommentaireModel.php
+
+// utilisez le typage si possible
+
+function addCommentaire(){
+
+}
+
+function readAllCommentaires(){
+
+}
+
+function countAllCommentaires(){
+    
+}
+
+// bonus pagination
+
+
+function readPaginationCommentaires(){
+
+}
