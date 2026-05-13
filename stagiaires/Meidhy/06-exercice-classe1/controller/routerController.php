53c5d00aa9ae3f86956bc6bd709fa66f51c0bccc
TNK-Sempaï <alioussalah.meidhy@gmail.com>
2026-05-13 10:27:13 +0200

    prep ex6

---
+++ stagiaires/Meidhy/06-exercice-classe1/controller/routerController.php
@@ -0,0 +1,8 @@
+<?php
+// stagiaires/Michael/06-exercice-classe1/controller/routerController.php
+
+# Importer le fichier model qui contient nos fonctions de la table commentaire
+
+# Création de notre connexion PDO (avec try catch)
+
+# suivant les actions utilisateur, appelez les vues.
---
+++ stagiaires/Meidhy/06-exercice-classe1/public/index.php
@@ -0,0 +1,6 @@
+<?php
+// stagiaires/Michael/06-exercice-classe1/public/index.php
+
+# Importation de ../config-dev.php 
+
+# Importation du router : ROOT_PROJECT."/controller/routerController.php"
