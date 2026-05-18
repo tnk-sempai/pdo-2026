dc6d25bd4538f8049b7a62c8739e9989a7c349be
LaetitiaBraun <laetitia.pianiste@gmail.com>
2026-05-18 15:38:05 +0200

    Mises à jour continue

--- stagiaires/Laetitia/06-exercice-classe1/controller/routerController.php
+++ stagiaires/Laetitia/06-exercice-classe1/controller/routerController.php
@@ -2,7 +2,14 @@
 // stagiaires/Michael/06-exercice-classe1/controller/routerController.php
 
 # Importer le fichier model qui contient nos fonctions de la table commentaire
+require ROOT_PROJECT."/model/CommentaireModel.php";
 
 # Création de notre connexion PDO (avec try catch)
+try{
+    $connectDB = new PDO(MARIA_DSN, DB_CONNECT_USER, DB_CONNECT_PWD);
+}catch(Exception $e){
+    die($e->getMessage());
+}
 
 # suivant les actions utilisateur, appelez les vues.
+include ROOT_PROJECT."/view/homepage.html.php";
--- stagiaires/Laetitia/06-exercice-classe1/view/homepage.html.php
+++ stagiaires/Laetitia/06-exercice-classe1/view/homepage.html.php
@@ -1,11 +1,33 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Page d'accueil</title>
 </head>
 <body>
-    <h1>Page d'accueil</h1>
+    <nav>
+        <ul>
+            <li><a href="view/homepage.html.php">Accueil</a></li>
+            <li><a href="model/CommentaireModel.php">Commentaires</a></li>
+            <li><a href="">Ajouter un commentaire</a></li>
+        </ul>
+    </nav>
+
+    <h1>Ma passion</h1>
+
+    <div class="container">
+        <img src="img/pexels-daven-hsu-5766928-35590036.jpg" alt="Image d'un piano" width="400px">
+        <img src="img/pexels-steve-860662.jpg" alt="Image d'un piano" width="400px">
+        <img src="img/pexels-sumeyyebasbil-20067759.jpg" alt="Image d'un piano" width="400px">
+    </div>
+
+    <p></p>
+
+    <ul>
+        <li><a href="model/CommentaireModel.php" class="black">Commentaires</a></li>
+        <li><a href="" class="black">Ajouter un commentaire</a></li>
+    </ul>
 </body>
 </html>
